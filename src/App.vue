21f5ec8a1a4aaf666aed1c47c8c7fe8c5f6db911
calm1205 <naginani072@gmail.com>
2023-08-05 08:22:54 +0900

    payment

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import Payment from "./components/Payment.vue";
 </script>
 
 <template>
   <h1>udemy vue</h1>
   <HelloWorld msg="Vite + Vue" />
+  <Payment />
 </template>
 
 <style scoped>
---
+++ src/components/Payment.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>最近の支出</h1>
+  <label> Payment.js </label>
+</template>
+
+<style scoped></style>
